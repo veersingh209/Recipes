e5c4a49eb3068238622bd5e5baafd2b0747d1540
Veer Singh <veersingh209@icloud.com>
2022-08-23 11:16:09 -0700

    Recipe Model

---
+++ Recipes/Recipes/Models/Recipe.swift
@@ -0,0 +1,22 @@
+//
+//  Recipe.swift
+//  Recipes
+//
+//  Created by Veer Singh on 8/23/22.
+//
+
+import Foundation
+
+class Recipe: Identifiable, Codable {
+    
+    var name: String
+    var featured: Bool
+    var image: String
+    var description: String
+    var prepTime: String
+    var totalTime: String
+    var servings: Int
+    var ingredients: [String]
+    var directions: [String]
+    
+}
