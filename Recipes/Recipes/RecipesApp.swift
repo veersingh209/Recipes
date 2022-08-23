85b132ef4a5ff134a109f343d5a48ab2fa05b44b
Veer Singh <veersingh209@icloud.com>
2022-08-23 11:08:47 -0700

    Xcode init

---
+++ Recipes/Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Veer Singh on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
