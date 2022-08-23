fadd641cb3981e007e37de15542d4e6141eb5157
Veer Singh <veersingh209@icloud.com>
2022-08-23 11:39:08 -0700

    Recipe List view

--- Recipes/Recipes/Views/RecipesListView.swift
+++ Recipes/Recipes/Views/RecipesListView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct RecipesListView: View {
+    
+    @ObservedObject var model = RecipeModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(model.recipe) { recipe in
+            HStack {
+                Image(recipe.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height: 60, alignment: .leading)
+                    .clipped()
+                    .cornerRadius(15)
+                VStack(alignment: .leading) {
+                    Text(recipe.name)
+                        .font(.title2)
+                    Text(recipe.description)
+                        .font(.caption)
+                }
+            }
+        }
     }
 }
 
